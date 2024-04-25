1e01dd71a2a7a0b6039b1a706bd93799e0549d59
Ali-AboEldahab <3liaboeldahab@gmail.com>
2024-04-25 17:14:03 +0200

    Home Page

--- HUc/Controllers/HomeController.cs
+++ HUc/Controllers/HomeController.cs
@@ -1,21 +1,34 @@
 using HUc.Models;
+using HUc.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace HUc.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HosinOldTestingContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(HosinOldTestingContext context, ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var viewModel = new CountsVM
+            {
+                StudentCount = await _context.UseresUsers.CountAsync(s => s.IsStaff == false),
+                EmployeeCount = await _context.UseresUsers.CountAsync(s => s.IsStaff == true),
+                BatchCount = await _context.DepartmentsYears.CountAsync(d => d.Year != null),
+                DeptCount = await _context.DepartmentsDepartments.CountAsync(d => d.Name != null)
+
+            };
+
+            return View(viewModel);
         }
 
         public IActionResult Privacy()
